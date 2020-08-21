4c4932e6680962ad456f6414e5b0a2de7a3b7fa4
karsongkester <karsonglenkester@gmail.com>
2020-08-21 12:54:14 -0600

    first

--- pageObjects/bustinPage.js
+++ pageObjects/bustinPage.js
@@ -1,7 +1,28 @@
 module.exports = {
     url: 'https://bustinboards.com/collections/who-makes-the-best-longboard-or-skateboard-you-do',
     elements: {
-
+        sportster:{
+        selector:'//a[contains(text(), "DESIGN SPORTSTER")]',
+        locateStrategy: 'xpath'
+        },
+        shrike: {
+        selector:'//a[contains(text(), "DESIGN SHRIKE")]',
+        locateStrategy: 'xpath'
+        },
+        maestro: {
+        selector:'//a[contains(text(), "DESIGN MAESTRO")]',
+        locateStrategy: 'xpath'
+        },
+        yoFace:{
+        selector: '//a[contains(text(), "DESIGN YOFACE")]',
+        locateStrategy: 'xpath'
+        },
+        finishBlue: {
+        selector:'//img[@data-reactid=".0.0.0.0.2.3.1.1.0.0.1.0"]',
+        locateStrategy: 'xpath'
+        },
+//here
+       
 
 
 
