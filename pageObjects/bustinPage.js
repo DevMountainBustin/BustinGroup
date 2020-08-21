3689b9e7e1600587d3ac5e25a298152cdaced641
Matt Pike <mpike_88@hotmail.com>
2020-08-21 15:41:38 -0600

    ugh

--- pageObjects/bustinPage.js
+++ pageObjects/bustinPage.js
@@ -2,16 +2,12 @@ module.exports = {
     url: 'https://bustinboards.com/collections/who-makes-the-best-longboard-or-skateboard-you-do',
     elements: {
 
-<<<<<<< HEAD
-        //Start page Buttons
-=======
 //Start page Buttons
->>>>>>> d93ffb481f0319d97a820cb5db70ece171808d01
         sportsterBtn: { selector: '//a[contains(text(), "DESIGN SPORTSTER")]', locateStrategy: 'xpath' },
         shrikeBtn: { selector: '//a[contains(text(), "DESIGN SHRIKE")]', locateStrategy: 'xpath' },
         maestroBtn: { selector: '//a[contains(text(), "DESIGN MAESTRO")]', locateStrategy: 'xpath' },
         yofaceBtn: { selector: '//a[contains(text(), "DESIGN YOFACE")]', locateStrategy: 'xpath' },
-        //Graphics choice Options
+//Graphics choice Options
         dropGraphic: {selector: '//a[@data-reactid=".0.0.0.0.2.1.0.0"]', locateStrategy: 'xpath'},
         lionGraphic: { selector: '//img[contains(@data-reactid,".0.0.0.0.2.1.1.1.0.0.1.0")]', locateStrategy: 'xpath' },
         djGraphic: { selector: '//img[contains(@data-reactid,".0.0.0.0.2.1.1.1.0.1.1.0")]', locateStrategy: 'xpath' },
@@ -23,40 +19,36 @@ module.exports = {
         statueGraphic: { selector: '//img[contains(@data-reactid,".0.0.0.0.2.1.1.1.0.7.1.0")]', locateStrategy: 'xpath' },
         bustinGraphic: { selector: '//img[contains(@data-reactid,".0.0.0.0.2.1.1.1.0.8.1.0")]', locateStrategy: 'xpath' },
         catgirlGraphic: { selector: '//img[contains(@data-reactid,".0.0.0.0.2.1.1.1.0.9.1.0")]', locateStrategy: 'xpath' },
-        //Background Stains
+//Background Stains
         dropStain: {selector: '//a[@data-reactid=".0.0.0.0.2.2.0.0"]', locateStrategy: 'xpath'},
         woodStain: {selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.0.1.0"]', locateStrategy: 'xpath'},
         whiteStain: {selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.1.1.0"]', locateStrategy: 'xpath'},
         blackStain: {selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.2.1.0"]', locateStrategy: 'xpath'},
         redStain: {selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.3.1.0"]', locateStrategy: 'xpath'},
         yellowStain: {selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.4.1.0"]', locateStrategy: 'xpath'},
-        //Artist finish Colors
+//Artist finish Colors
         dropFinish: {selector: '//a[@data-reactid=".0.0.0.0.2.3.0.0"]', locateStrategy: 'xpath'},
         finishBlue: {selector: '//img[@data-reactid=".0.0.0.0.2.3.1.1.0.0.1.0"]',locateStrategy: 'xpath'},
         finishGreen: {selector: '//img[@data-reactid=".0.0.0.0.2.3.1.1.0.1.1.0"]',locateStrategy: 'xpath'},
         finishRainbow: {selector: '//img[@data-reactid=".0.0.0.0.2.3.1.1.0.2.1.0"]',locateStrategy: 'xpath'},
         finishPurple: {selector: '//img[@data-reactid=".0.0.0.0.2.3.1.1.0.3.1.0"]',locateStrategy: 'xpath'},
         finishBlueLine: {selector: '//img[@data-reactid=".0.0.0.0.2.3.1.1.0.4.1.0"]',locateStrategy: 'xpath'},
-        //Truck Color
+//Truck Color
         dropTruck: {selector: '//a[@data-reactid=".0.0.0.0.2.4.0.0"]', locateStrategy: 'xpath'},
         blackTruck: {selector: '//img[@data-reactid=".0.0.0.0.2.4.1.1.0.0.1.0"]', locateStrategy: 'xpath'},
         blueTruck: {selector: '//img[@data-reactid=".0.0.0.0.2.4.1.1.0.1.1.0"]', locateStrategy: 'xpath'},
         greenTruck: {selector: '//img[@data-reactid=".0.0.0.0.2.4.1.1.0.2.1.0"]', locateStrategy: 'xpath'},
         goldTruck: {selector: '//img[@data-reactid=".0.0.0.0.2.4.1.1.0.3.1.0"]', locateStrategy: 'xpath'},
         noTruck: {selector: '//img[@data-reactid=".0.0.0.0.2.4.1.1.0.4.1.0"]', locateStrategy: 'xpath'},
-        //Wheel Color
+//Wheel Color
         dropWheel: {selector: '//a[@data-reactid=".0.0.0.0.2.5.0.0"]', locateStrategy: 'xpath'},
         clearWheel: {selector: '//img[@data-reactid=".0.0.0.0.2.5.1.1.0.0.1.0"]', locateStrategy: 'xpath'},
         blueWheel: {selector: '//img[@data-reactid=".0.0.0.0.2.5.1.1.0.1.1.0"]', locateStrategy: 'xpath'},
         orangeWheel: {selector: '//img[@data-reactid=".0.0.0.0.2.5.1.1.0.2.1.0"]', locateStrategy: 'xpath'},
         blackWheel: {selector: '//img[@data-reactid=".0.0.0.0.2.5.1.1.0.3.1.0"]', locateStrategy: 'xpath'},
         noWheel: {selector: '//img[@data-reactid=".0.0.0.0.2.5.1.1.0.4.1.0"]', locateStrategy: 'xpath'},
-<<<<<<< HEAD
-        //Bearing Selection
-=======
 //Bearing Selection
         dropBear: {selector: '//a[@data-reactid=".0.0.0.0.2.6.0.0"]', locateStrategy: 'xpath'},
->>>>>>> f9f9e3d9dee2b9f7db91624747e545ba64f7259e
         abec7Bear: {selector: '//a[@data-reactid=".0.0.0.0.2.6.1.1.0.0.1.0"]', locateStrategy: 'xpath'},
         abec9Bear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.1.1.0"]', locateStrategy: 'xpath'},
         abec9BuiltBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.2.1.0"]', locateStrategy: 'xpath'},
@@ -65,7 +57,7 @@ module.exports = {
         noBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.5.1.0"]', locateStrategy: 'xpath'},
         
         
-        
+
 
 
 
