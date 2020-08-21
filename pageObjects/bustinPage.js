dd37617e6e85a88730f5c8ff5909854915774b6b
CJ <cjtakawalk@yahoo.com>
2020-08-21 12:50:27 -0700

    Stains

--- pageObjects/bustinPage.js
+++ pageObjects/bustinPage.js
@@ -1,11 +1,27 @@
 module.exports = {
     url: 'https://bustinboards.com/collections/who-makes-the-best-longboard-or-skateboard-you-do',
     elements: {
-
-
-
-
-
+        //Background Stains
+        woodStain: {
+            selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.0.1.0"]',
+            locateStrategy: 'xpath'
+        },
+        whiteStain: {
+            selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.1.1.0"]',
+            locateStrategy: 'xpath'
+        },
+        blackStain: {
+            selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.2.1.0"]',
+            locateStrategy: 'xpath'
+        },
+        redStain: {
+            selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.3.1.0"]',
+            locateStrategy: 'xpath'
+        },
+        yellowStain: {
+            selector: '//img[@data-reactid=".0.0.0.0.2.2.1.1.0.4.1.0"]',
+            locateStrategy: 'xpath'
+        },
 
     }
 }
