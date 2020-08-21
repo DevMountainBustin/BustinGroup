3191c759a249e05200babe6b07075c51d0668df1
Matt Pike <mpike_88@hotmail.com>
2020-08-21 15:45:42 -0600

    fixing mistake

--- pageObjects/bustinPage.js
+++ pageObjects/bustinPage.js
@@ -54,9 +54,10 @@ module.exports = {
         abec9BuiltBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.2.1.0"]', locateStrategy: 'xpath'},
         sixballBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.3.1.0"]', locateStrategy: 'xpath'},
         ceramicBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.4.1.0"]', locateStrategy: 'xpath'},
-        noBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.5.1.0"]', locateStrategy: 'xpath'},
-        
-        
+        noBear: {selector: '//img[@data-reactid=".0.0.0.0.2.6.1.1.0.5.1.0"]', locateStrategy: 'xpath'},        
+//Cart
+        cartBtn: {selector: '//button[@data-reactid=".0.0.0.0.2.0.1.0"]', locateStrategy: 'xpath'},
+        submitBtn: {selector: '//button[@id="StickySubmit"]', locateStrategy: 'xpath'},        
 
 
 
